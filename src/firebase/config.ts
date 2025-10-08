205559a61cab37f1b6fac89096d4880a3f95cfbb
MestreJp <mestrejpfg@gmail.com>
2025-10-08 13:48:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6214754208-3ceec",
+  "appId": "1:974195956329:web:c58a2a27e72ba96ce820f8",
+  "apiKey": "AIzaSyAGlRMFnxhRxOMS4-6v0hGdNXpK3KThetE",
+  "authDomain": "studio-6214754208-3ceec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "974195956329"
+};
