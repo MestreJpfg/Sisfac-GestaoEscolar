88a6f440889eaa7012a2ba1b6d882a593aeca096
MestreJp <mestrejpfg@gmail.com>
2025-11-21 18:17:45 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/app/dashboard/subjects/page.tsx
+++ src/app/dashboard/subjects/page.tsx
@@ -1,13 +1,10 @@
 
 'use client';
 
-import AuthGuard from "@/components/auth-guard";
 import SubjectManager from "@/components/subject-manager";
 
 export default function SubjectsPage() {
   return (
-    <AuthGuard>
-      <SubjectManager />
-    </AuthGuard>
+    <SubjectManager />
   );
 }
