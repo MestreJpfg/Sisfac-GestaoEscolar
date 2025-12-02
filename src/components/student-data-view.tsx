0057077f5ab414fb3c4882d2ac2fabc429f9271a
MestreJp <mestrejpfg@gmail.com>
2025-12-02 00:25:36 +0000

    Vamos fazer o seguinte, vou dizer explicitamente o que tentar fazer: na

--- src/components/student-data-view.tsx
+++ src/components/student-data-view.tsx
@@ -42,7 +42,7 @@ export default function StudentDataView() {
   const debouncedNome = useDebounce(filters.nome, 300);
   const [sortConfig, setSortConfig] = useState<SortConfig>({ key: 'nome', direction: 'ascending' });
 
-  // Query for populating filter options
+  // Query for populating filter options. Fetches ALL students.
   const allStudentsForFiltersQuery = useMemoFirebase(() => firestore ? query(collection(firestore, 'alunos')) : null, [firestore]);
   const { data: allStudentsForFilters, isLoading: isLoadingAllStudents } = useCollection(allStudentsForFiltersQuery);
   
